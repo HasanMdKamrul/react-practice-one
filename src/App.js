9461470b1c7cac3172b40799bb95bdd2983c2ae8
HasanMdKamrul <kamrulhasan@iut-dhaka.edu>
2022-09-20 00:41:28 +0200

    Data fetching and show in the ui

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 // ** styles 
@@ -9,19 +9,71 @@ function App() {
  
   return (
     <div className="App">
-      <h1>Counter App</h1>
-      <Counter></Counter>
+      <h1>Extranal Users from Api</h1>
+      <ExtranalUsers></ExtranalUsers>
+      {/* <h1>Counter App</h1>
+      <Counter></Counter> */}
     </div>
   );
 };
 
 
+// ** Extranal user fetching component
+
+function ExtranalUsers(){ 
+  const [users,setUsers] = useState([]);
+  // ** fetching the data 
+
+  useEffect(()=>{
+    const dataLoader = async ()=>{
+      try {
+        const response = await fetch(`https://jsonplaceholder.typicode.com/users`);
+        response.ok ? console.log('Successfull') : console.log('failed');
+        const data = await response.json();
+        setUsers(data)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    dataLoader()
+  },[]);
+
+  return(
+    <div>
+      {
+        users.map(user => {
+          const {name,email} = user;
+          return <UserDisplay name={name} email={email}></UserDisplay>
+        })
+      }
+    </div>
+  )
+
+};
+
+const styles = {
+  border: '2px solid red',
+  margin: '15px',
+  borderRadius: '20px',
+  backgroundColor: 'beige',
+  padding: '5%'
+}
+
+const UserDisplay = ({name,email})=>{
+  return(
+    <div style={styles}>
+      <h1>Name:{name} </h1>
+      <p>Email:{email}</p>
+    </div>
+  )
+}
+
 // ** Functional counter component
 
 const Counter = ()=>{
+  // ** useState() => ekta array return kore [initialvalue, function] ei format a
   const [count,setCount] = useState(0);
-  
-  // ** Increase handler function
+ // ** Increase handler function
 
   const increaseHandler = ()=> setCount(count + 1);
   const decreaseHandler = ()=> setCount(count - 1);
